f75cf4f4d438a4038c58f7c7b07c552e94190bfd
Mabarik667f <jc.bob1412@gmail.com>
2024-05-04 00:41:05 +0300

    add django_extensions, fix docker-compose, add MEDIA_ROOT in urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
