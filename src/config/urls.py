c5ae37cab15942abb1e06b11b6a7ebe77d3b7e77
Mabarik667f <jc.bob1412@gmail.com>
2024-06-02 16:08:35 +0300

    fix: add DEBUG in url

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,7 +18,12 @@ from .settings import MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
